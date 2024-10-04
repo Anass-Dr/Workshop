6050939c6e4c69d7dbbfbd03cad5a6ecee713742
Anass-Dr <anasdrissi77@gmail.com>
2024-10-04 11:05:50 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (_, res) => res.send("home page"));
+app.get("/contact", (_, res) => res.send("contact page"));
+app.get("/login", (_, res) => res.send("login page"));
+
+module.exports = app;
---
+++ tests/app.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('Route Tests', () => {
+  
+  test('GET / should return home page', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('home page');
+  });
+
+  test('GET /contact should return contact page', async () => {
+    const response = await request(app).get('/contact');
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('contact page');
+  });
+
+  test('GET /login should return login page', async () => {
+    const response = await request(app).get('/login');
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('login page');
+  });
+
+});
