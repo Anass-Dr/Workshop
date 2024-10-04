42bbfec715685fa1aea66a1bfd9b0dcf23063d17
Anass-Dr <anasdrissi77@gmail.com>
2024-10-04 12:02:29 +0100

    added changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (_, res) => res.send("home page"));
+app.get("/", (_, res) => res.send("home page test"));
 app.get("/contact", (_, res) => res.send("contact page"));
 app.get("/login", (_, res) => res.send("login page"));
 
