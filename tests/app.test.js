70b5781c164481b210232d34c2b40f7c718c03f0
Anass-Dr <anasdrissi77@gmail.com>
2024-10-04 12:05:13 +0100

    added changes

--- tests/app.test.js
+++ tests/app.test.js
@@ -6,7 +6,7 @@ describe('Route Tests', () => {
   test('GET / should return home page', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('home page test');
+    expect(response.text).toBe('home page');
   });
 
   test('GET /contact should return contact page', async () => {
